a4641585657fac4ee72fa1c9b12e63790c0cc4ac
Tobias Böker <tb@softwarepioniere.de>
2020-10-28 10:42:41 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # minver-playground
 playing with minver
+
+```
+dotnet tool install --global minver-cli --version 2.3.1
+minver
+```
+
+```
+md take1
+cd take1
+dotnet new console
+dotnet sln ..\minver-playground.sln add .\take1.csproj
+dotnet add package MinVer
+dotnet run
+```
---
+++ take1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace take1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ass = typeof(Program).Assembly;
+
+            Console.WriteLine("Assembly.GetName().Version: {0}", ass.GetName().Version);
+            Console.WriteLine("AssemblyVersionAttribute: {0}", ((AssemblyVersionAttribute) ass.GetCustomAttribute(typeof(AssemblyVersionAttribute)))?.Version);
+            Console.WriteLine("AssemblyFileVersionAttribute: {0}", ((AssemblyFileVersionAttribute) ass.GetCustomAttribute(typeof(AssemblyFileVersionAttribute)))?.Version);
+        }
+    }
+}
