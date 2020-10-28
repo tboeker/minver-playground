f93eb35a3c1497e783544728f7bb276eb4cd988c
Tobias Böker <tb@softwarepioniere.de>
2020-10-28 10:51:44 +0100

    ..

--- take1/Program.cs
+++ take1/Program.cs
@@ -10,7 +10,7 @@ namespace take1
             var ass = typeof(Program).Assembly;
 
             Console.WriteLine("Assembly.GetName().Version: {0}", ass.GetName().Version);
-            Console.WriteLine("AssemblyVersionAttribute: {0}", ((AssemblyVersionAttribute) ass.GetCustomAttribute(typeof(AssemblyVersionAttribute)))?.Version);
+         //   Console.WriteLine("AssemblyVersionAttribute: {0}", ((AssemblyVersionAttribute) ass.GetCustomAttribute(typeof(AssemblyVersionAttribute)))?.Version);
             Console.WriteLine("AssemblyFileVersionAttribute: {0}", ((AssemblyFileVersionAttribute) ass.GetCustomAttribute(typeof(AssemblyFileVersionAttribute)))?.Version);
         }
     }
